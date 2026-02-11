28b1a8cef7d8f63af8354464f0cf0cae11061003
Lethicia Almeida <lethicia.almeida62@gmail.com>
2026-02-11 14:34:27 -0300

    Implantação do cadastro

--- SistemaDeNotas.c
+++ SistemaDeNotas.c
@@ -1,13 +1,11 @@
-#include <stdio.h> 
+#include <stdio.h>
 #include <string.h>
 
 #define N 40
 #define M 4
 
-//MENU - LETHICIA 
-
-
-int main() {
+int main()
+{
     char nomes[N][60];
     int id[N];
     int notas[N][M];
@@ -18,88 +16,113 @@ int main() {
     int opcao = 0;
     int i, j;
 
-    for(i = 0; i < N; i++) {
+    for (i = 0; i < N; i++)
+    {
         id[i] = -1;
         faltas[i] = 0;
         media[i] = 0;
-        for(j = 0; j < M; j++) {
+        for (j = 0; j < M; j++)
+        {
             notas[i][j] = -1;
         }
     }
-
-    do {
+            //MENU - LETHICIA 
+    do
+    {
         printf("\n===== SISTEMA DE GERENCIAMENTO DE TURMA =====\n");
         printf("\n===== MENU PRINCIPAL =====\n");
+
         printf("1. Cadastrar alunos\n");
-        printf("2. Lançar notas\n");
-        printf("3. Lançar faltas\n");
+        printf("2. Lancar notas\n");
+        printf("3. Lancar faltas\n");
         printf("4. Listar alunos\n");
         printf("5. Boletim completo\n");
-        printf("6. Estatísticas da turma\n");
+        printf("6. Estatisticas da turma\n");
         printf("7. Ranking Top 5\n");
         printf("8. Sair\n");
         printf("Escolha uma opcao: ");
         scanf("%d", &opcao);
 
-        switch(opcao) {
-            case 1: {
-                int q, k;
-                int idValido;
-                int repetido;
+        switch (opcao)
+        {
+        case 1:
+        {
+            int q, k;
+            int idValido;
+            int repetido;
 
-                printf("\nQuantos alunos deseja cadastrar? ");
-                scanf("%d", &q);
+            printf("\nQuantos alunos deseja cadastrar? ");
+            scanf("%d", &q);
 
-                if(q < 1 || q > N - totalAlunos) {
-                    printf("\nQuantidade invalida!\n");
-                } else {
-                    for(k = 0; k < q; k++) {
-                        printf("\nCadastro do aluno %d\n", totalAlunos + 1);
-                        do {
-                            idValido = 1;
-                            printf("Digite o ID (>0): ");
-                            scanf("%d", &id[totalAlunos]);
+            if (q < 1 || q > N - totalAlunos)
+            {
+                printf("\nQuantidade invalida ou limite da turma excedido!\n");
+            }
+            else
+            {
+                for (k = 0; k < q; k++)
+                {
+                    printf("\nCadastro do aluno %d\n", totalAlunos + 1);
 
-                            if(id[totalAlunos] <= 0) {
-                                printf("ID invalido!\n");
-                                idValido = 0;
-                            }
+                    idValido = 0;
+                    while (idValido == 0)
+                    {
+                        printf("Digite o ID (maior que 0): ");
+                        scanf("%d", &id[totalAlunos]);
 
+                        if (id[totalAlunos] <= 0)
+                        {
+                            printf("ID invalido! Deve ser positivo.\n");
+                        }
+                        else
+                        {
                             repetido = 0;
-                            for(i = 0; i < totalAlunos; i++) {
-                                if(id[i] == id[totalAlunos]) {
+                            for (i = 0; i < totalAlunos; i++)
+                            {
+                                if (id[i] == id[totalAlunos])
+                                {
                                     repetido = 1;
                                 }
                             }
 
-                            if(repetido) {
-                                printf("ID ja existe!\n");
-                                idValido = 0;
+                            if (repetido == 1)
+                            {
+                                printf("ID ja existe! Tente outro.\n");
                             }
-                        } while(!idValido);
-
-                        printf("Digite o nome: ");
-                        scanf(" %[^\n]", nomes[totalAlunos]);
-
-                        faltas[totalAlunos] = 0;
-                        media[totalAlunos] = 0;
-
-                        for(j = 0; j < M; j++) {
-                            notas[totalAlunos][j] = -1;
+                            else
+                            {
+                                idValido = 1; 
+                            }
                         }
-                        totalAlunos++;
                     }
+
+                    printf("Digite o nome: ");
+                    scanf(" %[^\n]", nomes[totalAlunos]);
+
+                    faltas[totalAlunos] = 0;
+                    media[totalAlunos] = 0;
+
+                    for (j = 0; j < M; j++)
+                    {
+                        notas[totalAlunos][j] = -1;
+                    }
+
+                    totalAlunos++;
+                    printf("Aluno cadastrado com sucesso!\n");
                 }
-                break;
             }
-            case 8:
-                printf("Saindo...\n");
-                break;
-            default:
-                if(opcao != 8) printf("Opcao invalida!\n");
-                break;
+            break;
+        }
+        case 8:
+            printf("Saindo...\n");
+            break;
+        default:
+            if (opcao != 8)
+                printf("Opcao invalida!\n");
+            break;
         }
 
-    } while(opcao != 8); 
-    return 0; 
-} 
+    } while (opcao != 8);
+
+    return 0;
+}
